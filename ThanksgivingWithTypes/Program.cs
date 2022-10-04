301968bccae9570b95f2ea32bc697aa19940ae2d
your-username <jenniej297@gmail.com>
2022-10-04 11:20:53 -0500

    initial commit

---
+++ ThanksgivingWithTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using ThanksgivingWithTypes;
+
+var thxgvng = new Thanksgiving();
+
+var thxgvngDay = Thanksgiving.Day.Thursday;
+
+thxgvng.Turkey = "Mr. Gobbleton";
+
+thxgvng.Food = new string[3] { "Green beans", "Mashed Potatoes", "Rolls" };
+
+thxgvng.TurkeyWeight = 12;
+
+Console.WriteLine($" Thanksgiving is always on {thxgvngDay}. \n");
+Console.WriteLine($"The turkey weighs {thxgvng.TurkeyWeight}lbs this year! \n");
+Console.WriteLine($"A few sides we will have with the turkey include: ");
+
+foreach (var item in thxgvng.Food)
+{
+    Console.WriteLine($" {item}");
+}
+Console.WriteLine($" \n We are calling the turkey, { thxgvng.Turkey}.");    
---
+++ ThanksgivingWithTypes/Thanksgiving.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanksgivingWithTypes
+{
+    public class Thanksgiving
+    {
+        public string[] Food;
+        public string Turkey { get; set; }  
+        public int? TurkeyWeight { get; set; }
+        public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+    }
+}
